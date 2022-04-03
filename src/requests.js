eccaad0144d7c975bd0848b67edcf4387625b79d
Shubham Kumar <shubhamkumarlhh@gmail.com>
2022-04-03 20:10:20 +0530

    initial commit

--- src/requests.js
+++ src/requests.js
@@ -22,7 +22,7 @@ const requests = [
     url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28&with_watch_monetization_types=flatrate`,
   },
   {
-    title: 'Popular Science-Fiction',
+    title: 'Science Fiction (Sci-fi)',
     url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=878&with_watch_monetization_types=flatrate`,
   },
   {
